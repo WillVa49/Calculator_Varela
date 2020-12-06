e0856d75e8901bcd74d715dd6c7094e8c46068c7
Wilfrido Varela <wilfrido.varela@my.tccd.edu>
2020-12-06 03:10:12 +0000

    Finished coding

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main()
 {
   double num1 = 0.0, num2 = 0.0;
   string operation = " ";
-
+  char answer = '\0';
+do
+{
   cout << "Please enter your first number: " << endl;
   num1 = validateDouble(num1);
   cout << "Please enter your second number: " << endl;
@@ -18,23 +20,35 @@ int main()
   if (operation == "add" || operation == "+")
   {
     cout << num1 << "+" << num2 << " = " << num1 + num2;
+    cout << "\nRun again? (1 = yes, 2 = no)";
+    cin >> answer;
   }
   else if (operation == "subtract" || operation == "-")
   {
     cout << num1 << "-" << num2 << " = " << num1 - num2;
+    cout << "\nRun again? (1 = yes, 2 = no)";
+    cin >> answer;
   }
   else if (operation == "multiply" || operation == "*")
   {
     cout << num1 << "*" << num2 << " = " << num1 * num2;
+    cout << "\nRun again? (1 = yes, 2 = no)";
+    cin >> answer;
   }
   else if (operation == "divide" || operation =="/")
   {
     cout << num1 << "/" << num2 << " = " << num1 / num2;
+    cout << "\nRun again? (1 = yes, 2 = no)";
+    cin >> answer;
   }
   else 
   {
     cout << "Error, please try again.";
   }
+  
+}while (answer != '2');
+cout << "Thank you for trying me, good bye. " << endl;
+
 
 return 0;
 }
